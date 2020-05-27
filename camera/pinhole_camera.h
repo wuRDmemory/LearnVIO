7747e69ea31d3781043ef89b04a3d8cedef4aa55
wuch <wuch2039@163.com>
2020-05-27 19:38:30 +0800

    add camera

--- camera/pinhole_camera.h
+++ camera/pinhole_camera.h
@@ -3,14 +3,44 @@
 #include <memory>
 #include "camera_base.h"
 
-class PinholeCamera : public CameraBase {
-public:
-    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+class PinholeCamera: virtual public CameraBase {
+    protected:
+        float fx_, fy_, cx_, cy_;
+        float d0_, d1_, d2_, d3_;
 
-    float fx_, fy_, cx_, cy_;
-    float k1_, k2_, p1_, p2_;
+        Eigen::Matrix3f K_, invK_;
+        Eigen::Matrix<float, 1, 4> D_;
 
+        bool mUseDistort;
+
+    public:
+        EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+
+        PinholeCamera(int width, int height, float fx, float fy, float cx, float cy, 
+                      float d0=0, float d1=0, float d2=0, float d3=0, float d4=0);
+        virtual ~PinholeCamera();
+
+        virtual Eigen::Vector3f cam2world(const float& x, const float& y) const;
+
+        virtual Eigen::Vector3f cam2world(const Eigen::Vector2f& px) const;
         
-};
+        virtual Eigen::Vector2f world2cam(const Eigen::Vector3f& xyz_c) const;
+
+        virtual Eigen::Vector2f world2cam(const Eigen::Vector2f& uv) const;
+
+        const Eigen::Vector2f focal_length() const { return Vector2f(fx_, fy_);}
+
+        virtual float errorMultiplier2() const { return fabs(fx_);}
+
+        virtual float errorMultiplier() const { return fabs(4.0*fx_*fy_); }
+
+        const Matrix3f& K()    const { return K_;    }
+
+        const Matrix3f& invK() const { return invK_; }
+
+        int initUnistortionMap();
+
+        int undistortImage(const cv::Mat& raw, cv::Mat& rectified);
+};;
 
 typedef shared_ptr<PinholeCamera> PinholeCameraPtr;
